c9dbc8ce76a043703aa652fd7bc8ca029849c3e5
Zan_23 <zasonce@gmail.com>
2022-05-22 12:13:02 +0200

    Refactoring - auto refactored some stuff with intellij to be compliant to pep8, renamed var - added plotly library to requirements.txt

--- src/data_loader.py
+++ src/data_loader.py
@@ -4,19 +4,19 @@ from pathlib import Path
 RAW_DATA_PATH = Path(__file__).parent.parent / "data" / "raw"
 
 EVENTMSGTYPE_dict = {
-    1 : "FIELD_GOAL_MADE",
-    2 : "FIELD_GOAL_MISSED",
-    3 : "FREE_THROW",
-    4 : "REBOUND",
-    5 : "TURNOVER",
-    6 : "FOUL",
-    7 : "VIOLATION",
-    8 : "SUBSTITUTION",
-    9 : "TIMEOUT",
-    10 : "JUMP_BALL",
-    11 : "EJECTION",
-    12 : "PERIOD_BEGIN",
-    13 : "PERIOD_END"
+    1: "FIELD_GOAL_MADE",
+    2: "FIELD_GOAL_MISSED",
+    3: "FREE_THROW",
+    4: "REBOUND",
+    5: "TURNOVER",
+    6: "FOUL",
+    7: "VIOLATION",
+    8: "SUBSTITUTION",
+    9: "TIMEOUT",
+    10: "JUMP_BALL",
+    11: "EJECTION",
+    12: "PERIOD_BEGIN",
+    13: "PERIOD_END"
 }
 
 EVENTMSGACTIONTYPE_FIELD_GOAL_dict = {
@@ -101,48 +101,55 @@ EVENTMSGACTIONTYPE_FIELD_GOAL_dict = {
 }
 
 EVENTMSGACTIONTYPE_FREE_THROW_dict = {
-    10 : "Free Throw 1 of 1",
-    11 : "Free Throw 1 of 2",
-    12 : "Free Throw 2 of 2",
-    13 : "Free Throw 1 of 3",
-    14 : "Free Throw 2 of 3",
-    15 : "Free Throw 3 of 3",
-    16 : "Free Throw Technical",
-    17 : "Free Throw Clear Path", #before 2006-07 season
-    18 : "Free Throw Flagrant 1 of 2",
-    19 : "Free Throw Flagrant 2 of 2",
-    20 : "Free Throw Flagrant 1 of 1",
-    21 : "Free Throw Technical 1 of 2",
-    22 : "Free Throw Technical 2 of 2",
-    23 : "Free Throw 1 of 2", #Guess
-    24 : "Free Throw 2 of 2", #Guess
-    25 : "Free Throw Clear Path 1 of 2", 
-    26 : "Free Throw Clear Path 2 of 2",
-    27 : "Free Throw Flagrant 1 of 3",
-    28 : "Free Throw Flagrant 2 of 3",
-    29 : "Free Throw Flagrant 3 of 3"
+    10: "Free Throw 1 of 1",
+    11: "Free Throw 1 of 2",
+    12: "Free Throw 2 of 2",
+    13: "Free Throw 1 of 3",
+    14: "Free Throw 2 of 3",
+    15: "Free Throw 3 of 3",
+    16: "Free Throw Technical",
+    17: "Free Throw Clear Path",  # before 2006-07 season
+    18: "Free Throw Flagrant 1 of 2",
+    19: "Free Throw Flagrant 2 of 2",
+    20: "Free Throw Flagrant 1 of 1",
+    21: "Free Throw Technical 1 of 2",
+    22: "Free Throw Technical 2 of 2",
+    23: "Free Throw 1 of 2",  # Guess
+    24: "Free Throw 2 of 2",  # Guess
+    25: "Free Throw Clear Path 1 of 2",
+    26: "Free Throw Clear Path 2 of 2",
+    27: "Free Throw Flagrant 1 of 3",
+    28: "Free Throw Flagrant 2 of 3",
+    29: "Free Throw Flagrant 3 of 3"
 }
 
-def load_data(columns = None, seasons = None, path = RAW_DATA_PATH, resolve = True):
+
+def load_data(columns=None, seasons=None, path=RAW_DATA_PATH, resolve=True):
     files = sorted([f for f in path.glob('*.csv')])
     season_dfs = []
-    for f in files:
-        #Only read specified seasons:
-        if seasons and (f.stem[:7] not in seasons):
+    for file_name in files:
+        # Only read specified seasons:
+        if seasons and (file_name.stem[:7] not in seasons):
             continue
-        season = pd.read_csv(f, usecols = columns)
-        season["season_name"] = f.stem[:7]
+        season = pd.read_csv(file_name, usecols=columns)
+        season["season_name"] = file_name.stem[:7]
         if resolve:
-            if columns == None or "EVENTMSGTYPE" in columns:
+            if columns is None or "EVENTMSGTYPE" in columns:
                 season["EVENTMSGTYPE"] = season["EVENTMSGTYPE"].replace(EVENTMSGTYPE_dict)
-            if columns == None or "EVENTMSGACTIONTYPE" in columns:
-                season.loc[season["EVENTMSGTYPE"]==EVENTMSGTYPE_dict[1],"EVENTMSGACTIONTYPE"] = season.loc[season["EVENTMSGTYPE"]==EVENTMSGTYPE_dict[1],"EVENTMSGACTIONTYPE"].replace(EVENTMSGACTIONTYPE_FIELD_GOAL_dict)
-            if columns == None or "EVENTMSGACTIONTYPE" in columns:
-                season.loc[season["EVENTMSGTYPE"]==EVENTMSGTYPE_dict[2],"EVENTMSGACTIONTYPE"] = season.loc[season["EVENTMSGTYPE"]==EVENTMSGTYPE_dict[2],"EVENTMSGACTIONTYPE"].replace(EVENTMSGACTIONTYPE_FIELD_GOAL_dict)
-            if columns == None or "EVENTMSGACTIONTYPE" in columns:
-                season.loc[season["EVENTMSGTYPE"]==EVENTMSGTYPE_dict[3],"EVENTMSGACTIONTYPE"] = season.loc[season["EVENTMSGTYPE"]==EVENTMSGTYPE_dict[3],"EVENTMSGACTIONTYPE"].replace(EVENTMSGACTIONTYPE_FREE_THROW_dict)
-        #remove empty columns:
+            if columns is None or "EVENTMSGACTIONTYPE" in columns:
+                season.loc[season["EVENTMSGTYPE"] == EVENTMSGTYPE_dict[1], "EVENTMSGACTIONTYPE"] = season.loc[
+                    season["EVENTMSGTYPE"] == EVENTMSGTYPE_dict[1], "EVENTMSGACTIONTYPE"].replace(
+                    EVENTMSGACTIONTYPE_FIELD_GOAL_dict)
+            if columns is None or "EVENTMSGACTIONTYPE" in columns:
+                season.loc[season["EVENTMSGTYPE"] == EVENTMSGTYPE_dict[2], "EVENTMSGACTIONTYPE"] = season.loc[
+                    season["EVENTMSGTYPE"] == EVENTMSGTYPE_dict[2], "EVENTMSGACTIONTYPE"].replace(
+                    EVENTMSGACTIONTYPE_FIELD_GOAL_dict)
+            if columns is None or "EVENTMSGACTIONTYPE" in columns:
+                season.loc[season["EVENTMSGTYPE"] == EVENTMSGTYPE_dict[3], "EVENTMSGACTIONTYPE"] = season.loc[
+                    season["EVENTMSGTYPE"] == EVENTMSGTYPE_dict[3], "EVENTMSGACTIONTYPE"].replace(
+                    EVENTMSGACTIONTYPE_FREE_THROW_dict)
+        # remove empty columns:
         if "NEUTRALDESCRIPTION" in season.columns:
             season = season.drop("NEUTRALDESCRIPTION", 1)
         season_dfs.append(season)
-    return pd.concat(season_dfs).reset_index(drop = True) if season_dfs else None
+    return pd.concat(season_dfs).reset_index(drop=True) if season_dfs else None
