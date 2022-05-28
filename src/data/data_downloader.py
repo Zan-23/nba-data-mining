be0951ca2af5b1e465c42b3654d0d49cdb743738
Zan_23 <zasonce@gmail.com>
2022-05-28 13:57:11 +0200

    Scraper for player bios - added new dependencies needed for scraping - implemented scraper for player bios, it starts up a chrome instance from which it extracts data - data is saved into a csv with all the meta information

--- src/data/data_downloader.py
+++ src/data/data_downloader.py
@@ -1,10 +1,20 @@
 import os
+import random
+import time
 from datetime import datetime
+
+import pandas as pd
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 from tqdm import tqdm
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 NBA_PAGE_URL = "https://eightthirtyfour.com"
+BASE_NBA_URL = "https://www.nba.com/stats/players/bio/"
 NBA_DATA_PAGE_URL = f"{NBA_PAGE_URL}/data"
 DATA_RAW_FOLDER = "./data/raw/"
 
@@ -78,10 +88,101 @@ def download_and_save_url_data(data_urls_arr):
     print(f"\nFinished data download in {execution_time}")
 
 
+def get_player_info_season_urls():
+    """
+    Method generates all the urls that contain data about players for seasons 2000-2020.
+    :return: Array of tuples. First element is the season (2002-2003) and the second values is the URL.
+    """
+    print(f"Started retrieving data  urls from '{BASE_NBA_URL}' website for seasons 2000-2020")
+
+    url_arr = []
+    # get all urls from 2000 to 2019
+    for i in range(2000, 2020):
+        season = f"{i}-{i+1}"
+        url = f"{BASE_NBA_URL}?Season={season}&SeasonType=Regular%20Season&PerMode=Totals"
+        url_arr.append((season, url))
+    return url_arr
+
+
+def scrape_player_bios_and_save_to_csv():
+    """
+    Function scrapes the player bios from multiple urls, combines the information to dataframes, combines the dataframes
+    for all seasons and save the information to a csv file.
+
+    Function involves a lot of waiting, because the page needs to load up and it also contains a random sleep
+    to avoid getting detected for scraping.
+    """
+    dir_path = f"{DATA_RAW_FOLDER}player-data"
+    start_time = time.time()
+
+    print(f"Started scraper for play info from '{BASE_NBA_URL}' website for season 2000-2020")
+    if os.path.isdir(dir_path) is False:
+        raise Exception(f"{dir_path} folder does not exist, but it should!")
+    output_csv_file = f"{dir_path}/player_info.csv"
+    url_arr = get_player_info_season_urls()
+
+    main_df = pd.DataFrame()
+    # Doesn't work with headless chrome, so we need to use a real browser, version 102 of chrome is needed
+    s_count = 1
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    for season_str, season_url in url_arr:
+        print(f"{s_count:02d}/{len(url_arr)} - Started scraping on '{season_url}' ...")
+        driver.get(season_url)
+        # wait to load the data
+        driver.implicitly_wait(5)
+        # Select to show all rows of players, gets the select element and changes the value to show all rows
+        selector_page_el = driver.find_elements(by=By.XPATH,
+                                                value="//select[contains(@class, 'stats-table-pagination__select')]")[0]
+        players_selector = Select(selector_page_el)
+        players_selector.select_by_visible_text("All")
+        # wait for the data to load
+        driver.implicitly_wait(5)
+
+        # get the webpage source and find relevant elements
+        page_source = driver.page_source
+        soup = BeautifulSoup(page_source, "html.parser")
+        main_data_table = soup.find_all("div", {"class": "nba-stat-table"})[0]
+
+        # get player IDs and name for each <a> element in table
+        player_id_name_dict = dict()
+        for a_el in soup.find_all("a", href=lambda value: value and value.startswith("/stats/player/")):
+            # if there are duplicate slashes replace them with a single slash
+            href_str = a_el["href"].replace("//", "/")
+            if href_str.startswith("/stats/player/") and href_str.endswith("/"):
+                player_id = href_str.split("/")[-2]
+                if player_id.isdigit():
+                    player_name = a_el.text.strip()
+                    if player_name in player_id_name_dict and player_id_name_dict[player_name] != player_id:
+                        raise Exception(f"Same player cannot have two different IDs: {player_name}! "
+                                        f"{player_id_name_dict[player_name]} != {player_id}")
+                    else:
+                        player_id_name_dict[player_name] = player_id
+        # convert dictionary to dataframe and prepare the columns for joining
+        player_id_df = pd.json_normalize(player_id_name_dict).T.reset_index()
+        player_id_df.columns = ["Player", "Player ID"]
+
+        # convert data from table to data frame and merge with player ID dataframe
+        data_frame = pd.read_html(str(main_data_table))[0].merge(player_id_df, on="Player")
+        data_frame["Season"] = season_str
+        main_df = pd.concat([main_df, data_frame], ignore_index=True)
+
+        print(f"{s_count:02d}/{len(url_arr)} - Finished scraping on '{season_url}', performing random sleep ...")
+        time.sleep(random.random() * 4)
+        s_count += 1
+
+    # quit the browser/selenium session
+    driver.quit()
+    print(f"\n\nSaving scraped data to a csv file {output_csv_file}")
+    main_df.to_csv(output_csv_file)
+    print(f"\nWhole scraping & saving process finished in {round(time.time() - start_time, 2)} seconds")
+
+
 def scraper_main():
-    os.path.join(os.path.dirname("./../.."))
     # get urls from the data page
     data_urls_arr = retrieve_data_urls_with_file_names()
+
+    # TODO also scrape for player bios?
+    # scrape_player_bios_and_save_to_csv()
 
     # download and save the data
     download_and_save_url_data(data_urls_arr)
@@ -89,5 +190,6 @@ def scraper_main():
 
 
 if __name__ == "__main__":
-    scraper_main()
+    scrape_player_bios_and_save_to_csv()
 
+    # scraper_main()
